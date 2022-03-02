2b7547ecd300a3fe5270584c4ded56e5f5e10823
Ly Kien Minh <lykienminh9@gmail.com>
2022-03-02 12:33:13 +0700

    add Allow-Methods

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const app = express();
 
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
 
 app.get('/', (req, res) =>{
     return res.json({"Message": "CyberSoft Compiler Hello!", "API": ["/getAllQuestionID", "/getAllQuestion", "/:id"]});
