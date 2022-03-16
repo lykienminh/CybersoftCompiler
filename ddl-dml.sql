d40bacca43d692c41a4577a6c370663ae90f9175
Ly Kien Minh <lykienminh9@gmail.com>
2022-03-16 15:03:30 +0700

    change to mssql server

--- ddl-dml.sql
+++ ddl-dml.sql
@@ -1,51 +1,63 @@
--- VERSION 1
+﻿-- VERSION 2
+
+--DROP DATABASE IF EXISTS db_cybersoft_compiler;
+--CREATE DATABASE [db_cybersoft_compiler];
+
+USE db_cybersoft_compiler;
+DROP TABLE IF EXISTS init_code;
+DROP TABLE IF EXISTS testcase;
+DROP TABLE IF EXISTS problem;
+
 CREATE TABLE problem (
-	question_id integer primary key,
-	question_title varchar(500), 
-	descriptions varchar(5000)
+	question_id INT PRIMARY KEY,
+	question_title VARCHAR(500), 
+	problem VARCHAR(1000),
+	input_format VARCHAR(500),
+	output_format VARCHAR(500),
+	explanation VARCHAR(1000),
+    constraint_input VARCHAR(500),
+    constraint_output VARCHAR(500),
+    constraint_time VARCHAR(500),
+	sample_input VARCHAR(500),
+	sample_output VARCHAR(500),
+    _level VARCHAR(50)
 );
 
 CREATE TABLE testcase (
-	question_id integer,
-	testcase_id integer,
-	_input varchar(500),
-	_output varchar(1000),
+	question_id INT,
+	testcase_id INT,
+	_input VARCHAR(500),
+	_output VARCHAR(1000),
 	PRIMARY KEY (question_id, testcase_id),
 	FOREIGN KEY (question_id) REFERENCES problem(question_id)
 );
 
 CREATE TABLE init_code (
-	question_id integer,
-	_language varchar(50),
-	base_code varchar(10000),
-	_function varchar(1000), 
-	_answer varchar(5000),
+	question_id INT,
+	_language VARCHAR(50),
+    tle INT,
+	base_code VARCHAR(8000),
+	_function VARCHAR(1000), 
+	_answer VARCHAR(5000),
 	PRIMARY KEY (question_id, _language),
 	FOREIGN KEY (question_id) REFERENCES problem(question_id)
 );
 
--- 21
-INSERT INTO problem
-VALUES (21, 'Unique Number', 
-'Given an array of integers in which every numbers appears twice except for one, your taks is to find that unique element.
-
-Expected : O(n) time, O(1) space
-
-Example:
-
-For arr = [19, 17, 19, 68, 68], the output should be uniqueNumber(arr) = 17.
-Input/Output:
-
-[Execution time limit] 1 seconds
-
-[Input] array.integer nums
-
-Guaranteed constraints:
+-- INSERT problem
+INSERT INTO problem VALUES (21, 'Unique Number', 'Given an array of integers in which every numbers appears twice except for one, your taks is to find that unique element.', 'array.integer nums', 'Integer', 'For arr = [19, 17, 19, 68, 68], the output should be uniqueNumber(arr) = 17.', 'Guaranteed constraints:
 1 ≤ nums.length < 104,
--109 ≤ nums[i] ≤ 109.
+-109 ≤ nums[i] ≤ 109.', 'integer', '1 seconds' ,'[19, 17, 19, 68, 68]', '17', 'easy');
 
-[Output] integer'); 
+INSERT INTO problem VALUES (22, 'Check Number', 'Given a string s containing characters and numbers, check to see if s is represented as a decimal integer or not. Returns 1 if true, -1 if false.', 'String str', 'Integer', 'For s = "a123"  then checkNumber(s) = false.
+Explanation: "a123" cannot be represented as a decimal integer.', 'str.Length ≤ 50', 'Return 1 if s is represented as a decimal integer, -1 otherwise.', '0.5s for C++, 3s for Java and C#, 4s for Python, Go and JavaScript.', '1365', '1', 'easy');
 
+INSERT INTO problem VALUES (23, 'Upper Case', 'Given a string str containing the name of a function, each single word is space-separated. Return the function name according to the UpperCase rule - Every single word must be capitalized first.', 'String str', 'String', 'For str = "hello case" then upper_case(str) = "HelloCase"
+For str = "camel case word" then upper_case(str) = "CamelCaseWord"', '0 < str.length ≤ 10^6', 'The function name follows the UpperCase principle', '0.5s for C++, 3s for Java and C#, 4s for  Python, Go and JavaScript.', 'test case', 'TestCase', 'easy');
+
+INSERT INTO problem VALUES (24, 'Check Max', 'Given a string str. Find the character which has the greatest decimal value in str (in ASCII table) and return that decimal value.', 'String str', 'Integer', 'For s1 = "abc", the output should be checkMax(s1)= 99.
+Because ''c'' has the greatest decimal value in s1 and the decimal value of ''c'' is 99.', '1 ≤ str.length ≤ 20000', 'The greatest decimal value.', '0.1s for C++, 0.6s for Java, C#, 0.8s for Python, Js, Go', 'abc', '99', 'easy');
+
+-- INSERT testcase
 INSERT INTO testcase VALUES (21, 1, '[19, 17, 19, 68, 68]', '17'); 
 INSERT INTO testcase VALUES (21, 2, '[34, 76, 76]', '34'); 
 INSERT INTO testcase VALUES (21, 3, '[46, 46, 11, 11, 59, 59, 55, 55, 35]', '35'); 
@@ -53,7 +65,23 @@ INSERT INTO testcase VALUES (21, 4, '[19, 1, 41, 41, 94, 1, 94, 80, 19]', '80');
 INSERT INTO testcase VALUES (21, 5, '[86, 51, 51, 72, 57, 57, 78, 78, 86]', '72'); 
 INSERT INTO testcase VALUES (21, 6, '[45, 90, 45, 90, 26, 38, 49, 26, 38]', '49'); 
 
-INSERT INTO init_code VALUES (21, 'cpp', 
+INSERT INTO testcase VALUES (22, 1, '1365', '1'); 
+INSERT INTO testcase VALUES (22, 2, 'ab2', '-1'); 
+INSERT INTO testcase VALUES (22, 3, '123412312441231312413134124', '1'); 
+
+INSERT INTO testcase VALUES (23, 1, 'test case', 'TestCase'); 
+INSERT INTO testcase VALUES (23, 2, 'camel case method', 'CamelCaseMethod'); 
+INSERT INTO testcase VALUES (23, 3, 'say hello', 'SayHello');
+INSERT INTO testcase VALUES (23, 4, ' camel case word', 'CamelCaseWord');
+INSERT INTO testcase VALUES (23, 5, '', '');
+
+INSERT INTO testcase VALUES (24, 1, 'abc', '99'); 
+INSERT INTO testcase VALUES (24, 2, 'fdhfghfgh', '104'); 
+INSERT INTO testcase VALUES (24, 3, 'slfjlJKHKJkk', '115'); 
+
+-- INSERT init_code
+--QUESTION 21
+INSERT INTO init_code VALUES (21, 'cpp', 1000,  
 '#include <iostream>
 #include <vector>
 #include <algorithm>
@@ -62,7 +90,7 @@ using namespace std;
 
 __function__
 
-int main() {
+int main(int argc, char** argv) {
     vector<int> param = {__test_case__};
     cout << uniqueNumber(param);
     return 0;
@@ -78,7 +106,7 @@ int main() {
         else return arr[i];
     }
 }'); 
-INSERT INTO init_code VALUES (21, 'java', 
+INSERT INTO init_code VALUES (21, 'java', 1000, 
 'import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -103,7 +131,7 @@ public class Solution {
 	}
 	return result;
 }'); 
-INSERT INTO init_code VALUES (21, 'js', 
+INSERT INTO init_code VALUES (21, 'js', 1000, 
 '__function__
 console.log(uniqueNumber([__test_case__]))', 
 'function uniqueNumber(arr) {
@@ -113,47 +141,23 @@ console.log(uniqueNumber([__test_case__]))',
         if (arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])) return arr[i]
     }
 }'); 
-INSERT INTO init_code VALUES (21, 'py', 
+INSERT INTO init_code VALUES (21, 'py', 1000, 
 '__function__
 print(uniqueNumber([__test_case__]))',
 'def uniqueNumber(arr):', 
 'def uniqueNumber(arr):
 	for x in arr:
 		if arr.count(x) == 1:
-			return x
-'); 
-
--- 22
-INSERT INTO problem
-VALUES (22, 'Check Number', 
-'Given a string s containing characters and numbers, check to see if s is represented as a decimal integer or not. Returns 1 if true, -1 if false.
-
-Example:
-
-For s = "a123"  then checkNumber(s) = false.
-Explanation: "a123" cannot be represented as a decimal integer.
-Input/Output:
-
-[Execution time] 0.5s for cpp, 3s for Java and C#, 4s for Python, Go and JavaScript.
-
-[Input]  String s
-s.Length ≤ 50
-
-[Output] Integer
-Return 1 if s is represented as a decimal integer, -1 otherwise.'); 
-
-INSERT INTO testcase VALUES (22, 1, '1365', '1'); 
-INSERT INTO testcase VALUES (22, 2, 'ab2', '-1'); 
-INSERT INTO testcase VALUES (22, 3, '123412312441231312413134124', '1'); 
-
-INSERT INTO init_code VALUES (22, 'cpp', 
+			return x'); 
+--QUESTION 22
+INSERT INTO init_code VALUES (22, 'cpp', 500, 
 '#include <iostream>
 
 using namespace std;
 
 __function__
 
-int main() {
+int main(int argc, char** argv) {
     cout << checkNumber("__test_case__");
     return 0;
 }', 
@@ -174,7 +178,7 @@ int main() {
         return 1;
     }
 }'); 
-INSERT INTO init_code VALUES (22, 'java', 
+INSERT INTO init_code VALUES (22, 'java', 3000, 
 'import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -190,16 +194,15 @@ public class Solution {
 'int checkNumber(String s) {
     return s.matches("^[-\\d]+$") ?  1 : -1;
 }'); 
-INSERT INTO init_code VALUES (22, 'js', 
+INSERT INTO init_code VALUES (22, 'js', 4000, 
 '__function__
-console.log(checkNumber("__test_case__"))
-', 
+console.log(checkNumber("__test_case__"))', 
 'function checkNumber(s) {
 }', 
 'function checkNumber(s) {
     return !Number(s) ? -1 : 1
 }'); 
-INSERT INTO init_code VALUES (22, 'py', 
+INSERT INTO init_code VALUES (22, 'py', 4000, 
 '__function__
 print(checkNumber("__test_case__"))',
 'def checkNumber(s):', 
@@ -208,45 +211,19 @@ print(checkNumber("__test_case__"))',
     	float(s)
     	return 1
     except:
-    	return -1
-');
-
--- 23
-INSERT INTO problem
-VALUES (23, 'Upper Case', 
-'Given a string str containing the name of a function, each single word is space-separated. Return the function name according to the UpperCase rule - Every single word must be capitalized first.
-
-Example:
-
-For str = "hello case" then upper_case(str) = "HelloCase".
-For str = "camel case word" then upper_case(str) = "CamelCaseWord".
-Input / Output:
-
-[Execution time] 0.5s for cpp, 3s for Java and C#, 4s for  Python, Go and JavaScript.
-[Input] String str
-0 < str.length ≤ 10^6
-
-[Output] String
-The function name follows the UpperCase principle'); 
-
-INSERT INTO testcase VALUES (23, 1, 'test case', 'TestCase'); 
-INSERT INTO testcase VALUES (23, 2, 'camel case method', 'CamelCaseMethod'); 
-INSERT INTO testcase VALUES (23, 3, 'say hello', 'SayHello');
-INSERT INTO testcase VALUES (23, 4, ' camel case word', 'CamelCaseWord');
-INSERT INTO testcase VALUES (23, 5, '', '');
-
-INSERT INTO init_code VALUES (23, 'cpp', 
+    	return -1');
+--QUESTION 23
+INSERT INTO init_code VALUES (23, 'cpp', 500, 
 '#include <iostream>
 
 using namespace std;
 
 __function__
 
-int main(){
+int main(int argc, char** argv){
     cout << upperCase("__test_case__");
     return 0;
-}
-', 
+}', 
 'string upperCase(string string) {
 }', 
 'string upperCase(string strings) {
@@ -260,7 +237,7 @@ int main(){
     }
     return str;
 }'); 
-INSERT INTO init_code VALUES (23, 'java', 
+INSERT INTO init_code VALUES (23, 'java', 3000, 
 'import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -270,8 +247,7 @@ public class Solution {
 	public static void main(String[] args) {
 		System.out.println(upperCase("__test_case__"));
 	}
-}
-', 
+}', 
 'String upperCase(String string) {
 }', 
 'String upperCase(String string) {
@@ -285,10 +261,9 @@ public class Solution {
 	}
 	return result;
 }'); 
-INSERT INTO init_code VALUES (23, 'js', 
+INSERT INTO init_code VALUES (23, 'js', 4000, 
 '__function__
-console.log(upperCase("__test_case__"));
-', 
+console.log(upperCase("__test_case__"));', 
 'function upperCase(string) {
 }', 
 'function upperCase(string) {
@@ -298,47 +273,24 @@ console.log(upperCase("__test_case__"));
     })
     .replace(/\s+/g, "");
 }'); 
-INSERT INTO init_code VALUES (23, 'py',
+INSERT INTO init_code VALUES (23, 'py', 4000, 
 '__function__
 print (upper_case("__test_case__"))',
 'def upper_case(string):', 
 'def upper_case(string):
-	return string.title().replace('' '', '''')
-');
-
--- 24
-INSERT INTO problem
-VALUES (24, 'Check Max', 
-'Given a string str. Find the character which has the greatest decimal value in str (in ASCII table) and return that decimal value.
-
-Example: 
-
-For s1 = "abc", the output should be checkMax(s1)= 99.
-Because ''c'' has the greatest decimal value in s1 and the decimal value of ''c'' is 99.
-Input/Output:
-
-[Execution time limit] 0.1s for cpp, 0.6s for Java, C#, 0.8s for Python, Js, Go
-[Input] string str
-1 ≤ s1.length ≤ 20000
-[Output]  integer
-The greatest decimal value.'); 
-
-INSERT INTO testcase VALUES (24, 1, 'abc', '99'); 
-INSERT INTO testcase VALUES (24, 2, 'fdhfghfgh', '104'); 
-INSERT INTO testcase VALUES (24, 3, 'slfjlJKHKJkk', '115'); 
-
-INSERT INTO init_code VALUES (24, 'cpp', 
+	return string.title().replace('' '', '''')');
+--QUESTION 24
+INSERT INTO init_code VALUES (24, 'cpp', 100, 
 '#include <iostream>
 
 using namespace std;
 
 __function__
 
-int main() {
+int main(int argc, char** argv) {
     cout << checkMax("__test_case__");
     return 0;
-}
-', 
+}', 
 'int checkMax(string str) {
 }', 
 'int checkMax(string str) {
@@ -347,7 +299,7 @@ int main() {
     if (str[i] > answer) answer = str[i];
     return answer;
 }'); 
-INSERT INTO init_code VALUES (24, 'java', 
+INSERT INTO init_code VALUES (24, 'java', 600, 
 'import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -357,8 +309,7 @@ public class Solution {
 	public static void main(String[] args) {
 		System.out.println(checkMax("__test_case__"));
 	}
-}
-', 
+}', 
 'int checkMax(String str) {
 }', 
 'int checkMax(String str) {
@@ -370,10 +321,9 @@ public class Solution {
 	}
 	return (int)max;
 }'); 
-INSERT INTO init_code VALUES (24, 'js', 
+INSERT INTO init_code VALUES (24, 'js', 800, 
 '__function__
-console.log(checkMax("__test_case__"))
-', 
+console.log(checkMax("__test_case__"))', 
 'function checkMax(str) {
 }', 
 'function checkMax(str) {
@@ -385,7 +335,7 @@ console.log(checkMax("__test_case__"))
     }
     return max;
 }'); 
-INSERT INTO init_code VALUES (24, 'py',
+INSERT INTO init_code VALUES (24, 'py', 800, 
 '__function__
 print(checkMax("__test_case__"))', 
 'def checkMax(str):',
@@ -394,5 +344,4 @@ print(checkMax("__test_case__"))',
 	ascii = []
 	for x in lst:
     	ascii.append(ord(x))
-	return max(ascii)
-');
+	return max(ascii)');
